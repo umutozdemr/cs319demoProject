e9094d518e0dd2ae81a12dd1aff201a0b78f9c72
Umut Ege Özdemir <88161164+umutozdemr@users.noreply.github.com>
2021-12-26 23:44:55 +0300

    adsfasd

--- src/main/front/allActivitiesAxios.js
+++ src/main/front/allActivitiesAxios.js
@@ -9,14 +9,15 @@ const getData = () => {
     axios.get('https://projectdeneme.herokuapp.com/activities/listActivities').then(function (response) {
     console.log(response);
         for(var i = 0; i < response.data.length; i++) {
-            var activityName = response.data[i].activityName;
+            var activityname = response.data[i].activityName;
+            var activitydesc = response.data[i].activityDescription;
             var activitydate = response.data[i].date;
             var activityplace = response.data[i].place;
             var accap = response.data[i].capacity;
             var ge250 = response.data[i].ge250Point;
             document.body.innerHTML = document.body.innerHTML + '<ul class="list-group list-group-horizontal mb-2 mt-4 "  >\n' +
-                '      <li class="list-group-item  col-lg-2 col-sm-2 col-md-2 col-4 text-center" id="activityClub1" style="border: none"> </li>\n' +
-                '      <li class="list-group-item  col-lg-2 col-sm-2 col-md-2 col-4 text-center" style="border: none"><span id="activityName1"> '+ activityName+ ' </span></li>\n' +
+                '      <li class="list-group-item  col-lg-2 col-sm-2 col-md-2 col-4 text-center" id="activityClub1" style="border: none"><span id="activityName1"> '+ activityname+ ' </span> </li>\n' +
+                '      <li class="list-group-item  col-lg-2 col-sm-2 col-md-2 col-4 text-center" style="border: none"><span id="activityName1"> '+ activitydesc+ ' </span></li>\n' +
                 '      <li class="list-group-item  col-lg-2 col-sm-2 col-md-2 col-4 text-center" style="border: none"><span id="activityDate1"> '+ activitydate+ ' </span></li>\n' +
                 '      <li class="list-group-item  col-lg-2 col-sm-2 col-md-2 col-4 text-center" style="border: none"><span id="activityPlace1"> '+ activityplace+ ' </span></li>\n' +
                 '      <li class="list-group-item  col-lg-1 col-sm-2 col-md-2 col-4 text-center" style="border: none"><span id="activityQuota1"> '+ accap+ '</span></li>\n' +
